99b24982867899b83c04ea74cdac20c44d08a8b7
sean.corcoran <sean.corcoran@crowdhelix.com>
2022-09-02 16:18:39 +0100

    initial commit - Laravel  TDD

---
+++ ticketbeast/tests/features/ViewConcertListingTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewConcertListingTest extends TestCase
+{
+  /** @test */
+  function user_can_view_a_concert_listing() {
+      //Arrange
+
+        // create a concert
+
+
+      //Act
+
+        // view the concert
+
+      //Assert                                           
+      
+        // see the concert details
+    
+  }
+}
